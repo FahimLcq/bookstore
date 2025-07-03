69046a49f9a9b965be84b94b4b8302deb590acec
Fahim Leclercq <fahim.leclercq@mousquetaires.com>
2025-07-04 00:01:20 +0200

    initial commit

---
+++ src/types/book.ts
@@ -0,0 +1,10 @@
+export type Book = {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  condition: string;
+  category: string;
+  images: string[];
+  created_at: string;
+};
